457fb37c25a56ccb42a1257918aa679387268e00
Diego Oliveira <estefano_costa_@hotmail.com>
2023-05-04 15:51:30 -0300

    Estilização

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -4,7 +4,7 @@ export default function PokemonCard({ name, id, img, types }) {
     // função para impressão de tipos
     return (
         <ul>
-            <li>
+            <li className={types[0].type.name}>
                 <img src={img} alt={name} />
                 <section>
                     <span>#{id < 10 ? `0${id}` : id + 2}</span>
