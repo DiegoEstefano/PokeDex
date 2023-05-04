71367c483d57bc66b2184654b981449ffa9c042b
Diego Oliveira <estefano_costa_@hotmail.com>
2023-05-04 14:05:54 -0300

    estilização de types

--- src/components/Types.jsx
+++ src/components/Types.jsx
@@ -1,13 +1,14 @@
+import '../components/Types.css'
 export default function Types({ types }) {
     const handleTypes = () => {
         if (types[1]) {
             return <section className='types'>
-                <div className='type'>{types[0].type.name}</div>
-                <div className='type'>{types[1].type.name}</div>
+                <div className={types[0].type.name}>{types[0].type.name}</div>
+                <div className={types[1].type.name}>{types[1].type.name}</div>
             </section>
         }
         return <section className='types'>
-            <div className='type'>{types[0].type.name}</div>
+            <div className={types[0].type.name}>{types[0].type.name}</div>
         </section>
     }
     return (handleTypes())
