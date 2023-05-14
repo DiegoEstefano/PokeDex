808c23604d75015b7995cc136036392c741f6982
Diego Oliveira <estefano_costa_@hotmail.com>
2023-05-14 15:07:38 -0300

    conclusõ box shadown classes

--- src/components/Types.jsx
+++ src/components/Types.jsx
@@ -3,12 +3,12 @@ export default function Types({ types }) {
     const handleTypes = () => {
         if (types[1]) {
             return <section className='types'>
-                <div  className={`tipo1  ${types[0].type.name}`}>{types[0].type.name}</div>
+                <div  className={`tipo1 ${types[0].type.name}`}>{types[0].type.name}</div>
                 <div className={`tipo2 ${types[1].type.name}`}>{types[1].type.name}</div>
             </section>
         }
         return <section  className='types'  >
-            <div className={`tipo1  ${types[0].type.name}`}>{types[0].type.name}</div>
+            <div className={`tipo1 ${types[0].type.name}`}>{types[0].type.name}</div>
         </section>
     }
     return (handleTypes())
