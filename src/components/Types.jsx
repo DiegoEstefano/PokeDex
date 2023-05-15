fba6aaa0b265eca516aaddf9452df904e24a1b21
Diego Oliveira <estefano_costa_@hotmail.com>
2023-05-15 08:17:06 -0300

    teste

--- src/components/Types.jsx
+++ src/components/Types.jsx
@@ -2,6 +2,7 @@ import '../components/Types.css'
 export default function Types({ types }) {
     const handleTypes = () => {
         if (types[1]) {
+            
             return <section className='types'>
                 <div className={`tipo1 ${types[0].type.name}`}>{types[0].type.name}</div>
                 <div className={`tipo2 ${types[1].type.name}`}>{types[1].type.name}</div>
